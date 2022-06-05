7a64b238de9150b463244ec3b7846520fe264200
Mike <michael.p.silverman@gmail.com>
2022-06-05 08:29:12 -0400

    edit painting name

--- server/routes/paintings/editPainting.js
+++ server/routes/paintings/editPainting.js
@@ -1,27 +1,36 @@
-const parseFormFieldsAndFiles = require('../utils/parseFormFieldsAndFiles')
-const Painting = require('../../models/painting')
-const assignPaintingImageFromFile = require('../utils/paintings/assignPaintingImageFromFile')
-const updateSeriesMetadata = require('../utils/series/updateSeriesMetadata')
+const parseFormFieldsAndFiles = require("../utils/parseFormFieldsAndFiles");
+const Painting = require("../../models/painting");
+const assignPaintingImageFromFile = require("../utils/paintings/assignPaintingImageFromFile");
+const updateSeriesMetadata = require("../utils/series/updateSeriesMetadata");
 
 exports.edit = async (req, res) => {
-  const parsedForm = await parseFormFieldsAndFiles(req)
+  const parsedForm = await parseFormFieldsAndFiles(req);
   try {
-    const paintingToBeEdited = await Painting.findOne({ title: parsedForm.fields.title })
-    if (!paintingToBeEdited) return res.status(400).json({ error: { message: "No painting found by that name" } })
-    Object.keys(parsedForm.fields).forEach(key => {
-      if (key !== "thumbnail" && key !== "image") paintingToBeEdited[key] = parsedForm.fields[key]
-    })
-    paintingToBeEdited.seriesList = parsedForm.fields.seriesList.split(",")
-    const image = parsedForm.files.image
+    const paintingToBeEdited = await Painting.findOne({
+      title: parsedForm.fields.uneditedTitle,
+    });
+    if (!paintingToBeEdited)
+      return res
+        .status(400)
+        .json({ error: { message: "No painting found by that name" } });
+    Object.keys(parsedForm.fields).forEach((key) => {
+      if (key !== "thumbnail" && key !== "image" && key !== "uneditedTitle")
+        paintingToBeEdited[key] = parsedForm.fields[key];
+    });
+    paintingToBeEdited.seriesList = parsedForm.fields.seriesList.split(",");
+    const image = parsedForm.files.image;
     if (image) {
-      await assignPaintingImageFromFile(paintingToBeEdited, parsedForm.files.image)
-      await createAndAssignThumbnailToPainting(paintingToBeEdited)
+      await assignPaintingImageFromFile(
+        paintingToBeEdited,
+        parsedForm.files.image
+      );
+      await createAndAssignThumbnailToPainting(paintingToBeEdited);
     }
-    await paintingToBeEdited.save()
-    updateSeriesMetadata(paintingToBeEdited.seriesList)
-    return res.status(200).json(paintingToBeEdited)
+    await paintingToBeEdited.save();
+    updateSeriesMetadata(paintingToBeEdited.seriesList);
+    return res.status(200).json(paintingToBeEdited);
   } catch (error) {
-    console.log(error)
-    return res.status(400).json(error)
+    console.log(error);
+    return res.status(400).json(error);
   }
-}
+};
